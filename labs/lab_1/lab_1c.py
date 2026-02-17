f1e0ea9ecbb33f5e68fb8dd2be1292a49cc592f6
Heona <heonaliu@gmail.com>
2026-02-17 16:44:12 -0500

    fix some formatting

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -5,13 +5,16 @@ Given a list of numbers, return the maximum sum of any contiguous subarray of th
 
 Do not assume anything. Account for all edge cases.
 
-Derived from LeetCode problem: https://leetcode.com/problems/maximum-subarray/ (leetcode medium)
+Derived from LeetCode problem:
+ https://leetcode.com/problems/maximum-subarray/ (leetcode medium)
 """
 
-# TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
+# TODO: Find and resolve the bug in the following implementation. 
+# Create unit tests to verify your fix.
 def max_subarray_sum(nums: list[int]) -> int:
     """
-    Function that takes in a list of integers and returns the maximum sum of any contiguous subarray.
+    Function that takes in a list of integers 
+    and returns the maximum sum of any contiguous subarray.
 
     Args:
         nums (list[int]): List of integers.
--- labs/lab_1/lab_1d.py
+++ labs/lab_1/lab_1d.py
@@ -1,17 +1,22 @@
 """
 lab_1d.py
 
-Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
+Given an array of integers `nums` and an integer `target`, 
+return indices of the two numbers such that they add up to `target`.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
 
 Derived from LeetCode problem: https://leetcode.com/problems/two-sum/ (leetcode easy)
 """
 
-# TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
+# TODO: Find and resolve the bug in the following implementation. 
+# Create unit tests to verify your fix.
 def two_sum(nums: list[int], target: int) -> list[int]:
     """
-    Function that takes in a list of integers and a target integer, and returns the indices of the two numbers that add up to the target.
+    Function that takes in a list of integers 
+    and a target integer, 
+    and returns the indices of the two numbers that add up to the target.
 
     Args:
         nums (list[int]): List of integers.
